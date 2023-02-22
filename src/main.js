aadb9bb128b260dede550b28a184e97b9d8718a4
@khan13 <aabdulaxadkhan@gmail.com>
2023-02-22 19:26:30 +0500

    vuex -i State  G M A M

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
 import uiComponents from './ui-components'
 
 // import './assets/main.css'
@@ -8,4 +9,5 @@ const app = createApp(App)
 
 uiComponents.map(component => app.component(component.name, component))
 app.use(router)
+app.use(store)
 app.mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,19 @@
 <template>
     <div>
-        Home
+       <h1> Home {{ counterAA }}</h1>
+        <div class="w-25">
+            <Button @click="incrementHandler">increment</Button>
+            <Button @click="decementHandler">decement</Button>
+        </div>
     </div>
 </template>
 
 <script>
+import store from '../store';
+import Button from '../ui-components/Button.vue';
+
 export default {
+    components: { Button },
     name: 'KhanVue3Home',
 
     data() {
@@ -19,8 +27,19 @@ export default {
     },
 
     methods: {
-        
+        incrementHandler(){
+           this.$store.commit('increment')
+        },
+        decementHandler(){
+            this.$store.commit('decrement')
+        }
     },
+
+    computed:{
+        counterAA() {
+            return this.$store.state.count
+        }
+    }
 };
 </script>
 
