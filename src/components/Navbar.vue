2923e605b78da1a08b85d16af4573f90b78f66a6
Abdulahad Azimov <aabdulaxadkhan@gmail.com>
2023-09-07 13:49:59 +0500

    tozalash

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,7 +3,6 @@
         <a class="d-flex align-items-center text-dark text-decoration-none">
             <img :src="logo" alt="logo" @click="toHomeHandler">
         </a>
-        <!-- {{ user }} -->
         <nav class="d-inline-flex mt-2 mt-md-0 ms-md-auto">
             <template v-if="isLoggedIn">
                 <RouterLink class="me-3 py-2 text-dark text-decoration-none" :to="{ name: 'home' }">
