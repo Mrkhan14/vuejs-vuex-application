dc43941fbbf96c69855a20c1a665ba2fbba5c44a
Abdulahad Azimov <aabdulaxadkhan@gmail.com>
2023-11-26 23:33:26 +0500

    add si

--- src/components/ArticlesCard.vue
+++ src/components/ArticlesCard.vue
@@ -5,9 +5,13 @@
                 style="height: 280px; width: 100%; display: block;">
             <div class="card-body">
                 <h5 class="card-title">{{ articleProps.title.slice(0, 50) }}...</h5>
+
                 <p class="card-text">{{ articleProps.description.slice(0, 100) }}...</p>
+
                 <p class="text-muted">{{ new Date(articleProps.createdAt).toLocaleString('uzb') }}</p>
+
                 <button @click="navigeteHendler" class="btn btn-primary" style="margin-right: 10px;">Show</button>
+
                 <button
                     style="margin-right: 10px;"
                     @click="navigeteEditHendler"
